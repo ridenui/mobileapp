32115a28c1bc7fbcea3574c53f5e59b4fdc3d111
Tom <me@dunklestoast.de>
2021-11-29 21:51:04 +0100

    feat: add basic navigation (#1)

---
+++ src/screens/login/Login.screen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+export function LoginScreen() {
+  return (
+    <SafeAreaView>
+      <Text>Login!</Text>
+    </SafeAreaView>
+  );
+}
