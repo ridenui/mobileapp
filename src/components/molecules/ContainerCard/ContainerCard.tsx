1a9fb21be5873d37017f0563811a28c4d5d60852
Tom <me@dunklestoast.de>
2022-02-17 17:37:49 +0100

    fix: use default icon as fallback

--- src/components/molecules/ContainerCard/ContainerCard.tsx
+++ src/components/molecules/ContainerCard/ContainerCard.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Typography, TypographyVariants } from '@atoms/Typography/Typography';
+import { DefaultIcon } from '@molecules/ContainerCard/DefaultIcon';
 import type { Container } from '@ridenui/unraid/dist/modules/docker/container';
 import * as S from './ContainerCard.styled';
 
@@ -11,18 +12,12 @@ export interface ContainerCardProps {
  * Description of ContainerCard.
  */
 export function ContainerCard({ container }: ContainerCardProps): JSX.Element {
-  const [image, setImage] = useState('');
+  const [image, setImage] = useState(`data:image/png;base64,${DefaultIcon}`);
 
   useEffect(() => {
-    container
-      .getImage()
-      .then((img) => {
-        setImage(`data:image/png;base64,${img}`);
-      })
-      .catch((e) => {
-        console.log(container.name);
-        console.log(e);
-      });
+    container.getImage().then((img) => {
+      setImage(`data:image/png;base64,${img}`);
+    });
   }, [container]);
 
   return (
